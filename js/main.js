91d0fe09957c4d903b3350a81117cb45d4fd4724
diegorosas19 <diegorosas19@gmail.com>
2021-04-18 15:04:00 -0300

    Aula 01 -introdução ao Javascript.

---
+++ js/main.js
@@ -0,0 +1,17 @@
+var nome = "Diego Rosas";
+var idade = 28;
+var idade2 = 20;
+var n1 = 5;
+var n2 = 3;
+var frase = "Japão é o melhor time do mundo!"
+//alert("Meu primeiro js");
+//alert("Bem vindo " + nome);
+//alert(nome + " tem " + idade + " anos!");
+//alert(idade + idade2);
+console.log(nome);
+console.log(idade + idade2);
+console.log(frase.replace("Japão","Brasil"));
+//alert(frase.replace("Japão","Brasil"));
+//console.log(frase.toUpperCase());
+//console.log(frase.toLowerCase());
+console.log(n1 * n2);
