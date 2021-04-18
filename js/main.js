856cfc51449a27f44a471327072441c374e6e1aa
diegorosas19 <diegorosas19@gmail.com>
2021-04-18 17:45:46 -0300

    Aula 02 - Arrays e Dicionario(objetos).

--- js/main.js
+++ js/main.js
@@ -1,17 +1,47 @@
-var nome = "Diego Rosas";
-var idade = 28;
-var idade2 = 20;
-var n1 = 5;
-var n2 = 3;
-var frase = "Japão é o melhor time do mundo!"
+//---------- Aula 02 ----------------------
+
+//-------- Discionario --------------------
+
+var frutas = [{nome:"maça", cor:"veremelha"}, {nome:"uva", cor:"roxa"}]
+console.log(frutas);
+console.log(frutas[1].nome);
+//console.log(fruta.cor);
+
+/*var fruta = {nome:"maça", cor:"veremelha"}
+console.log(fruta);
+console.log(fruta.nome);
+console.log(fruta.cor);
+*/
+
+//----------Arrays---------------------
+//var lista = ["maça", "pêra", "laranja"];
+//lista.push("uva"); // add uva a lista
+//lista.pop();
+//alert(lista[1]);
+//console.log(lista);
+//console.log(lista.length);
+//console.log(lista.reverse());
+//console.log(lista.toString());
+//console.log(lista.toString()[1]);
+//console.log(lista.join(" - "));
+
+
+
+//--------------Aula 01 --------------------
+//var nome = "Diego Rosas";
+//var idade = 28;
+//var idade2 = 20;
+//var n1 = 5;
+//var n2 = 3;
+//var frase = "Japão é o melhor time do mundo!"
 //alert("Meu primeiro js");
 //alert("Bem vindo " + nome);
 //alert(nome + " tem " + idade + " anos!");
 //alert(idade + idade2);
-console.log(nome);
-console.log(idade + idade2);
-console.log(frase.replace("Japão","Brasil"));
+//console.log(nome);
+//console.log(idade + idade2);
+//console.log(frase.replace("Japão","Brasil"));
 //alert(frase.replace("Japão","Brasil"));
 //console.log(frase.toUpperCase());
 //console.log(frase.toLowerCase());
-console.log(n1 * n2);
+//console.log(n1 * n2);
